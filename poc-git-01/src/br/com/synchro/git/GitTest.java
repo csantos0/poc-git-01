3b766fcde6a705f150bec40f7df66471e5f715f9
csantos0 <ciro.santos@synchro.com.br>
2014-12-22 17:18:25 -0200

    Initial Commit

---
+++ poc-git-01/src/br/com/synchro/git/GitTest.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.synchro.git;
+
+/**
+ * @author $author
+ * @version $revision
+ * @create $date
+ */
+public class GitTest {
+
+    public static void main(final String[] args) {
+
+	System.out.println("Git tutorial...");
+
+    }
+}
