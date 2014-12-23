e7e8ffa063c0ca816a01cc0c1bdc85ae969002eb
Ciro Santos <ciro.santos@synchro.com.br>
2014-12-23 09:28:27 -0200

    remoting test

---
+++ poc-git-01/src/br/com/synchro/git/GitRemoteTest.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.com.synchro.git;
+
+/**
+ * @author $author
+ * @version $revision
+ * @create $date
+ */
+public class GitRemoteTest {
+
+    public static void main(final String[] args) {
+	System.out.println("GIT Remote Test...");
+    }
+}
