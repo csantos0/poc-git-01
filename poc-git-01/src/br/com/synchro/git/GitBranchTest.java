7f0f0ae2f857cca6ed7e542a50766bef68f07a2c
Ciro Santos <ciro.santos@synchro.com.br>
2014-12-23 09:02:33 -0200

    commiting branch

---
+++ poc-git-01/src/br/com/synchro/git/GitBranchTest.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.com.synchro.git;
+
+/**
+ * @author $author
+ * @version $revision
+ * @create $date
+ */
+public class GitBranchTest {
+
+    public static void main(final String[] args) {
+	System.out.println("Branching...");
+    }
+}
